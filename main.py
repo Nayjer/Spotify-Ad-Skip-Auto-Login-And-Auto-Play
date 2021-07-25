4726437c890f38c75128391926d393c81c175c73
Nayjer <58821444+Zentraxus@users.noreply.github.com>
2021-07-25 21:59:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import time
 
 while True:
   os.system("ad_skipper.py")
+  time.sleep(2)
   
 #this file is needed, because everytime an ad is found, the other file gets closed, it needs an re-opening
